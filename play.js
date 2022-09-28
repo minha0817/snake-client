8af64345363669b74373a35b4b331f113b1a7b68
minha0817 <minhakk24@gmail.com>
2022-09-28 13:05:59 -0700

    exported connect function in client.js and required in play.js

--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    //conn object is an instance of the socket class.
-    //node's net library - function
-    host: "165.227.47.243", // for creating an object named conn.
-    port: 50541,
-  });
-  conn.setEncoding("utf-8");
-
-  conn.on("connect", () => {
-    console.log("Successfully connected");
-  });
-
-  conn.on("data", () => {
-    console.log("you ded cuz you idled");
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting.....");
 connect();
