75777fe31d44133cc89fba2083820dc54126c241
minha0817 <minhakk24@gmail.com>
2022-09-28 15:25:48 -0700

    created constants file to keep static data

--- client.js
+++ client.js
@@ -1,11 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    //conn object is an instance of the socket class.
-    //node's net library - function
-    host: "165.227.47.243", // for creating an object named conn.
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
   conn.setEncoding("utf-8");
 
