27309b8915309e1fe746f63dd4f936cb75ea139a
minha0817 <minhakk24@gmail.com>
2022-09-28 14:14:58 -0700

    comment out hard coded Move up

--- client.js
+++ client.js
@@ -12,6 +12,10 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: @MH");
+    // conn.write("Move: up");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
   });
 
   conn.on("data", () => {
